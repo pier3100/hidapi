80f5821c42bded15f2277aa9fb4010778b7edff7
Pier3150 <158572251+pier3100@users.noreply.github.com>
2024-05-14 15:12:33 +0200

    changed last dummy

--- hidapi_parser/hidapi_parser.c
+++ hidapi_parser/hidapi_parser.c
@@ -1547,7 +1547,7 @@ int hid_parse_input_elements_values( unsigned char* buf, int size, struct hid_de
                 
             } {
                 res = HidP_GetUsageValue(HidP_Input, cur_element->usage_page, 0, cur_element->usage, &new_value, pp_data, buf, report_length);
-                new_value = cur_element->report_index;
+                new_value = new_value + (10 * cur_element->report_index);
             }
             
             
