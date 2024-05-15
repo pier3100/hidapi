16bd3b4f6dbb6603c587fa65f9c2b0e242af1df5
Pier3150 <158572251+pier3100@users.noreply.github.com>
2024-05-15 23:52:14 +0200

    changed buffer size to 56, since this is the size of the index

--- hidapi_parser/hidapi_parser.c
+++ hidapi_parser/hidapi_parser.c
@@ -1536,8 +1536,8 @@ int hid_parse_input_elements_values( unsigned char* buf, int size, struct hid_de
                 int number_characters;
                 number_characters = cur_element->report_index;
                 number_characters = 1;
-                char buffer[13];
-                res = HidP_GetUsageValueArray(HidP_Input, cur_element->usage_page, 0, cur_element->usage, &buffer[0], 13, pp_data, buf, report_length);// TODO this is not yet correct, also the next section which considers the status, only makes sense for the base case, furtjermore we need to think about how to output this character array as c++ is strongly typed
+                char buffer[56];
+                res = HidP_GetUsageValueArray(HidP_Input, cur_element->usage_page, 0, cur_element->usage, &buffer[0], 56, pp_data, buf, report_length);// TODO this is not yet correct, also the next section which considers the status, only makes sense for the base case, furtjermore we need to think about how to output this character array as c++ is strongly typed
                 unsigned long loopCounter;
                 unsigned long number;
                 double factor;
@@ -1547,7 +1547,7 @@ int hid_parse_input_elements_values( unsigned char* buf, int size, struct hid_de
                     factor = (double) pow(10, (((double) number_characters - 1) - loopCounter));
                     new_value = (unsigned long) new_value + (factor * number); 
                 }
-                new_value = 1000 + cur_element->report_index;
+                new_value = 1000 + new_value;
             } else {
                 res = HidP_GetUsageValue(HidP_Input, cur_element->usage_page, 0, cur_element->usage, &new_value, pp_data, buf, report_length);
             }
