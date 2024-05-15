ab5739de251a9740c358b58da064938e884db99b
Pier3150 <158572251+pier3100@users.noreply.github.com>
2024-05-15 23:07:09 +0200

    testing with one character

--- hidapi_parser/hidapi_parser.c
+++ hidapi_parser/hidapi_parser.c
@@ -1536,7 +1536,7 @@ int hid_parse_input_elements_values( unsigned char* buf, int size, struct hid_de
                 //cur_element->report_index = 2; //for first test
                 int number_characters;
                 number_characters = cur_element->report_index;
-                number_characters = 2;
+                number_characters = 1;
                 char buffer[13];
                 res = HidP_GetUsageValueArray(HidP_Input, cur_element->usage_page, 0, cur_element->usage, &buffer[0], 13, pp_data, buf, report_length);// TODO this is not yet correct, also the next section which considers the status, only makes sense for the base case, furtjermore we need to think about how to output this character array as c++ is strongly typed
                 unsigned long loopCounter;
@@ -1547,9 +1547,8 @@ int hid_parse_input_elements_values( unsigned char* buf, int size, struct hid_de
                     number = buffer[loopCounter] - 48; //ansci to integer conversion
                     factor = (double) pow(10, (((double) number_characters - 1) - loopCounter));
                     new_value = (unsigned long) new_value + (factor * number); 
-                    new_value = 1000 + new_value;
                 }
-                
+                new_value = 1000 + new_value;
             } else {
                 res = HidP_GetUsageValue(HidP_Input, cur_element->usage_page, 0, cur_element->usage, &new_value, pp_data, buf, report_length);
             }
